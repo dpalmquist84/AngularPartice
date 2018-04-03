982640c00b32250bd39f95fe7b1487e30a731e55
DavidPalmquist <dpalmquist84@gmail.com>
2018-04-03 12:12:48 -0400

    made a new service

--- main.js
+++ main.js
@@ -1,51 +1,56 @@
-var app = angular.module("myApp", []);
+(function() {
+  var app = angular.module("githubViewer", []);
 
-app.controller("myCtrl", function($scope, $http, $interval, $log, $anchorScroll, $location){
-    
-    var onUserComplete = function(response) {
-        $scope.user = response.data;
-        console.log($scope.user);
-        $http.get($scope.user.repos_url)
-            .then(onRepos, onError);
+  var MainController = function(
+    $scope,
+    github,
+    $interval,
+    $log,
+    $anchorScroll,
+    $location
+  ) {
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError);
     };
 
-    var onRepos =function(response) {
-        $scope.repos = response.data;
-        $location.hash = "userDetails";
-        $anchorScroll();
-    }
+    var onRepos = function(data) {
+      $scope.repos = data;
+      $location.hash("userDetails");
+      $anchorScroll();
+    };
 
-    var onError = function(reason){
-        $scope.error = "Couldn't fetch data";
-    }
-    
-    $scope.search = function(username) {
-        $log.info("searching for " + username);
-   $http.get('https://api.github.com/users/' + username)
-    .then(onUserComplete)
-        if(countdownInterval) {
-            $interval.cancel(countdownInterval)
-             $scope.countdown = null;
-        }
-    }
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the data.";
+    };
 
-    var decrementCountdown =function(){
-        $scope.countdown -= 1;
-        if($scope.countdown < 1) {
-            $scope.search($scope.username);
-           
-        }
-    }
+    var decrementCountdown = function() {
+      $scope.countdown -= 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    };
+
     var countdownInterval = null;
-    const startCountdown = () => {
-       countdownInterval =  $interval(decrementCountdown, 1000, $scope.countdown)
-    }
+    var startCountdown = function() {
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
 
+    $scope.search = function(username) {
+      $log.info("Searching for " + username);
+      github.getUser(username).then(onUserComplete, onError);
+      if (countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
+    };
 
+    $scope.username = "angular";
+    $scope.message = "GitHub Viewer";
+    $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = 5;
+    startCountdown();
+  };
 
-$scope.username;
-$scope.message;
-$scope.repoSortOrder;
-$scope.countdown = 5;
-startCountdown();
-});
+  app.controller("MainController", MainController);
+})();
